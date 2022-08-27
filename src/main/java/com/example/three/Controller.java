dc1ae035ed0a8ce6e0b587608555bb3d825395a5
Binzoo <binamrathapa015@gmail.com>
2022-08-27 13:43:02 +0200

    Initial commit

---
+++ src/main/java/com/example/three/Controller.java
@@ -0,0 +1,96 @@
+package com.example.three;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Random;
+
+public class Controller {
+
+
+    @FXML
+    Label label;
+    @FXML
+    TextField textField;
+
+    @FXML
+    TextField textPassword;
+
+    public void generate(ActionEvent event) {
+
+
+        String text = textField.getText();
+        int value = 0;
+        try{
+             value = Integer.parseInt(text);
+        }catch (Exception e){
+            textPassword.setText("Cannot add letters. Please add numbers: ");
+        }
+
+        StringBuilder store = new StringBuilder();
+        String numberOfCharacter = "";
+
+        for (int j = 0; true; ) {
+            if (value >= 8 && value <= 30) {
+                for (int i = 0; i < 8; i++) {
+                    store.append(randomString()).append(randomInt()).append(randomString()).append(randomCapitalString()).append(randomCharacter());
+                }
+                numberOfCharacter = store.substring(0, value);
+                textPassword.setText(numberOfCharacter);
+                break;
+            } else {
+                if(value < 8){
+                    textPassword.setText(value + " numbers of character is unsafe.");
+                    break;
+                }else {
+                    textPassword.setText(value + " numbers of character is too much");
+                    break;
+                }
+            }
+        }
+    }
+
+    public static String randomInt() {
+        Random rand = new Random();
+        String alphabet = "1234567890";
+        String store = "";
+
+        store += alphabet.charAt(rand.nextInt(alphabet.length()));
+
+        return store;
+    }
+
+    // this method give random String
+    public static String randomString() {
+
+        Random rand = new Random();
+        String alphabet = "abcdefghijklmnopqrstuvwxyxz";
+        String store = "";
+
+        store += alphabet.charAt(rand.nextInt(alphabet.length()));
+
+        return store;
+    }
+
+    public static String randomCapitalString() {
+        Random rand = new Random();
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String store = "";
+
+        store += alphabet.charAt(rand.nextInt(alphabet.length()));
+
+        return store;
+    }
+
+    public static String randomCharacter() {
+        Random rand = new Random();
+        String alphabet = "!@#$%^&*()_+{}::L><?~`±§";
+        String store = "";
+
+        store += alphabet.charAt(rand.nextInt(alphabet.length()));
+
+        return store;
+    }
+}
